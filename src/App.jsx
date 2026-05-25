6f09db0145572f3db978994840d869372a6c635a
Edward <nintenderboy@gmail.com>
2026-05-26 01:18:49 +0200

    Initial commit: skirt animation with wind effects

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SkirtAnimation from './components/SkirtAnimation';
+
+function App() {
+  return (
+    <div className="app">
+      <SkirtAnimation />
+    </div>
+  );
+}
+
+export default App;
