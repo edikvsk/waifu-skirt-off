bc35b6445d9db2bf72d93754e9e85d350038edfc
Edward Vasilkovskii <edwardv@medialooks.com>
2026-06-01 14:56:41 +0200

    Improve 1st lvl environment

--- src/components/Environment3D.jsx
+++ src/components/Environment3D.jsx
@@ -3,6 +3,30 @@ import React from 'react';
 const Environment3D = () => {
   return (
     <>
+      {/* Фон японской школы */}
+      <div className="school-background">
+        {/* Задняя стена с окнами */}
+        <div className="back-wall">
+          <div className="window window-1"></div>
+          <div className="window window-2"></div>
+          <div className="window window-3"></div>
+          <div className="window window-4"></div>
+        </div>
+        
+        {/* Классная доска */}
+        <div className="blackboard">
+          <div className="blackboard-frame"></div>
+          <div className="blackboard-surface"></div>
+        </div>
+        
+        {/* Боковые стены */}
+        <div className="side-wall left"></div>
+        <div className="side-wall right"></div>
+        
+        {/* Потолок */}
+        <div className="ceiling"></div>
+      </div>
+
       {/* 3D пол */}
       <div className="floor-3d">
         <div className="floor-grid"></div>
